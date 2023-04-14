031e92b5cbd0b491052e3627203f52f539d2e5f1
Anthony Sanchez <antsanchez198@gmail.com>
2023-04-14 01:28:37 -0400

    finished product

--- src/App.js
+++ src/App.js
@@ -92,43 +92,43 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Total Clicks <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mouse" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="white" fill="none" stroke-linecap="round" stroke-linejoin="round">
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <rect x="6" y="3" width="12" height="18" rx="4" />
-          <line x1="12" y1="7" x2="12" y2="11" />
-        </svg> : {clickAmount}</h1>
+      <h1>Total Clicks: {clickAmount} <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mouse" width="24" height="24" viewBox="0 0 24 24" stroke-width="3" stroke="#75E6DA" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <rect x="6" y="3" width="12" height="18" rx="4" />
+        <line x1="12" y1="7" x2="12" y2="11" />
+      </svg></h1>
       <button onClick={() => { incrementClicks(); getClickAmount(); }}>Click Me!
       </button>
 
-      <h1>Clicks Around the Globe
-
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-map-pin" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="white" fill="none" stroke-linecap="round" stroke-linejoin="round">
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <circle cx="12" cy="11" r="3" />
-          <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
-        </svg>
-      </h1>
-      <div class="tbl-content">
-        <table cellpadding="0" cellspacing="0" border="0">
-          <thead>
-            <tr>
-              <th>Country</th>
-              <th>Clicks</th>
-            </tr>
-          </thead>
-          <tbody>
-            {dbData && dbData.map((item) => {
-              return (
-                <tr>
-                  <td>{item.Country}</td>
-                  <td>{item.Clicks}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+      <div className='bottom-container'>
+        <h1>Clicks Around the Globe
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-map-pin" style={{ marginLeft: "5px" }} width="24" height="24" viewBox="0 0 24 24" stroke-width="3" stroke="#75E6DA" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <circle cx="12" cy="11" r="3" />
+            <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+          </svg>
+        </h1>
+        <div class="tbl-content">
+          <table cellpadding="0" cellspacing="0" border="0">
+            <thead>
+              <tr>
+                <th>Country</th>
+                <th>Clicks</th>
+              </tr>
+            </thead>
+            <tbody>
+              {dbData && dbData.map((item) => {
+                return (
+                  <tr>
+                    <td>{item.Country}</td>
+                    <td>{item.Clicks}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
-
     </div>
   );
 }
